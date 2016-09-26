7ff5f2035397f340875a9307d8e34fe0586734f4
urosgolobrp <uros.golob@redpitaya.com>
2016-09-26 16:24:27 +0200

    set latching relays into SDR mode
    With this we set relays connected to RP into mode applicable in SDR.

--- projects/sdr_transceiver_hpsdr/bazaar/start.sh
+++ projects/sdr_transceiver_hpsdr/bazaar/start.sh
@@ -2,7 +2,7 @@ devcfg=/sys/devices/soc0/amba/f8007000.devcfg
 test -d $devcfg/fclk/fclk0 || echo fclk0 > $devcfg/fclk_export
 echo 1 > $devcfg/fclk/fclk0/enable
 echo 143000000 > $devcfg/fclk/fclk0/set_rate
-
+hvlv -s
 server=/opt/redpitaya/www/apps/sdr_transceiver_hpsdr/sdr-transceiver-hpsdr
 
 if aplay --list-devices | grep 'card 0:'
